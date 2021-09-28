6d2adba67a0596dcef84d682d2cfc466a1299bf8
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-28 19:35:12 +0200

    Prova Push

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App Mannaggia a voi mi avete fatto formattare il pc !"/>
   </div>
 </template>
 
